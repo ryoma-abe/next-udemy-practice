07fcd7c5e0de516fa7c204f066657b2847ca6aab
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-26 10:32:53 +0900

    クライアントコンポーネントにカウント機能を追加し、サーバーコンポーネントの表示を日本語に変更しました。

--- src/app/rcc/page.tsx
+++ src/app/rcc/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { useState } from "react";
+
 export default function ClientComponent() {
+  const [count, setCount] = useState(0);
   console.log("ClientComponent");
 
-  return <div>ClientComponent</div>;
+  return (
+    <div>
+      <p>ClientComponent</p>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+    </div>
+  );
 }
