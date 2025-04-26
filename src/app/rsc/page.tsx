2eb3d4f3b76d86b714c154df76cb2b4e1a97aefb
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-26 11:02:32 +0900

    サーバーコンポーネントにリンクを追加し、Aboutページへのナビゲーションを実装しました。

--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,10 +1,12 @@
 import ClientComponent from "@/components/ClientComponent";
+import Link from "next/link";
 
 export default function ServerComponent() {
   return (
     <div>
       <p>サーバー</p>
       <ClientComponent />
+      <Link href="/about">About</Link>
     </div>
   );
 }
