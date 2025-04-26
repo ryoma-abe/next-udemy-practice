413aa9e00d6fbdc964435ce3152943848b466291
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-26 20:40:19 +0900

    連絡先フォームのバリデーションを追加し、ユーザー入力の検証結果に基づいてエラーメッセージをコンソールに表示する処理を実装しました。

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -1,11 +1,18 @@
 "use server";
 
+import { ContactSchema } from "@/validations/contact";
 import { redirect } from "next/navigation";
 
 export async function submitContactForm(formData: FormData) {
   const name = formData.get("name");
   const email = formData.get("email");
 
+  const validationResult = ContactSchema.safeParse({ name, email });
+  if (!validationResult.success) {
+    const errors = validationResult.error.flatten();
+    console.log("サーバー側でエラーが発生", errors);
+    return {};
+  }
   console.log({ name, email });
   redirect("/contacts/complete");
 }
