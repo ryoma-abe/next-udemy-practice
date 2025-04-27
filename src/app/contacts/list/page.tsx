4562a21fac49173452133b0be03522e5084cf6a9
ryoma-abe <liaozhenanbu387@gmail.com>
2025-04-27 14:17:01 +0900

    連絡先リストページを非同期関数に変更し、連絡先の取得機能を追加しました。複数の連絡先をリスト表示し、最初の連絡先の名前を表示する機能を実装しました。また、データベースファイルが更新されました。

--- src/app/contacts/list/page.tsx
+++ src/app/contacts/list/page.tsx
@@ -1,3 +1,20 @@
-export default function ListPage() {
-  return <div>page</div>;
+import { getContact, getContacts } from "@/lib/contact";
+
+export default async function ListPage() {
+  const contacts = await getContacts();
+  const first = await getContact("1");
+  return (
+    <div>
+      複数
+      <ul>
+        {contacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.email}:{contact.name}
+          </li>
+        ))}
+      </ul>
+      1件
+      <div>{first ? first.name : "登録されていません"}</div>
+    </div>
+  );
 }
